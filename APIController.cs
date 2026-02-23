775f6c91b38870b618e35025a4617227f56d000a
jackson magas <jackson.magas53@gmail.com>
2026-02-22 21:03:23 -0500

    Added Steam Top 5 Games List From Account

--- APIController.cs
+++ APIController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SteamRater.Models
 {
-    public class SteamID
+    public class Root
     {
         public ResponseData Response { get; set; }
     }
@@ -16,11 +16,21 @@ namespace SteamRater.Models
     public class ResponseData
     {
         public List<Player> Players { get; set; }
+        public List<Game> Games { get; set; }
     }
 
     public class Player
     {
-        public string steamid { get; set; } = string.Empty;
+        public string personaname { get; set; } = string.Empty;
+    }
+
+    public class Game
+    {
+        public int appid { get; set; }
+        public string name { get; set; } = string.Empty;
+        public Double playtime_forever { get; set; }
+        public Double playtime_disconnected { get; set; }
+
     }
 }
 
@@ -36,26 +46,61 @@ namespace SteamRater.Pages
             _httpClientFactory = httpClientFactory;
         }
 
-        public Player Player1 = new Player();
+        public ResponseData GamesToken = new ResponseData();
+        public ResponseData PlayerToken = new ResponseData();
 
         public async Task OnGetAsync()
         {
             try
             {
+                // API to query steam family share instead of just steam profile owned games (my id: 76561198308578397, Emily: 76561199043218536)
                 var client = _httpClientFactory.CreateClient();
-                client.BaseAddress = new System.Uri("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=143AFF0EF09A8CF4A63A88A102C79E9E&steamids=76561198308578397");
+                // Keep base address shortened and use jsonasync to add pages and params
+                client.BaseAddress = new System.Uri("https://api.steampowered.com/");
+
+                Root data1 = await client.GetFromJsonAsync<Root>("ISteamUser/GetPlayerSummaries/v0002/?key=143AFF0EF09A8CF4A63A88A102C79E9E&steamids=76561198308578397&format=json");
+
+                if (data1?.Response?.Players != null)
+                {
+                    PlayerToken.Players = data1.Response.Players;
+                    Console.WriteLine(PlayerToken.Players[0].personaname);
+                }
 
                 // Call API and deserialize JSON into List<Post>
-                SteamID data = await client.GetFromJsonAsync<SteamID>("");
+                Root data2 = await client.GetFromJsonAsync<Root>("IPlayerService/GetOwnedGames/v1/?key=143AFF0EF09A8CF4A63A88A102C79E9E&steamid=76561198308578397&include_appinfo=true&include_played_free_games=true&appids_filter=&format=json");
 
-                if (data?.Response?.Players != null)
+                if (data2?.Response?.Games.Count != 0)
                 {
-                    foreach (var player in data.Response.Players)
-                    {
-                        Player1.steamid = player.steamid;
-                        Console.WriteLine($"Player ID: {player.steamid}");
-                    }
+                    GamesToken.Games = data2.Response.Games;
                 }
+
+                foreach (Game game in GamesToken.Games)
+                {
+                    // Add with playtime disconnected for real total time
+                    game.playtime_forever += game.playtime_disconnected;
+                    game.playtime_forever = Math.Round(game.playtime_forever / 60, 1);
+                }
+
+                // sort by playtime (Use Linq)
+                GamesToken.Games.Sort((g1, g2) => g2.playtime_forever.CompareTo(g1.playtime_forever));
+
+                Console.WriteLine(GamesToken.Games.Count);
+
+                //restrict list to top 5
+                while (GamesToken.Games.Count > 5)
+                {
+                    Console.WriteLine(GamesToken.Games[5].name);
+                    GamesToken.Games.RemoveAt(5);                    
+                }
+
+                Console.WriteLine(GamesToken.Games.Count);
+
+                /*
+                if (Games.Games.Count > 5)
+                {
+                    Games.Games = Games.Games.GetRange(0, 5);
+                }
+                */
             }
             catch (HttpRequestException ex)
             {
